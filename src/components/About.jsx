8aaa15799e9949f63e3d7b135f2f74d0663461da
kyoshie <2202901@ub.edu.ph>
2024-07-30 20:12:55 +0800

    about page done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,22 @@
 import React from 'react'; 
-
+import { ReactTyped } from "react-typed";
 
 const About = () => {
   return (
-    <div className='text-center'>
-      <h1 className='text-white'> Tuklas</h1>
-      <p className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus maiores iure rerum nemo, 
-        quia rem eligendi est ipsum reprehenderit eveniet nesciunt sunt ducimus odit eaque magnam deserunt, labore voluptatem excepturi!</p>
+    <div className='text-white'>
+        <div className='max-w-[800px] my-[-120px] w-full mx-auto text-center flex flex-col justify-center mt-[130px]'>
+          <div className='flex items-center justify-center mt-[20%] xl:mt-0 lg:mt-0'>
+          <h1 className='text-3xl font-bold md:text-4xl sm:text-2xl md:py-6 xl:text-6xl'>What is</h1>
+          <ReactTyped className='pl-2 text-3xl text-[--orange] md:text-4xl sm:text-4xl font-bold xl:text-6xl'
+                     strings={['Tuklas?']} typeSpeed ={90} backSpeed={100} loop/>
+          </div>
+          <p className='text-base font-medium text-white md:text-2xl xl:text-4xl'>Tuklas is a Filipino word means <span className='text-[--orange]'>Discover</span> in english.
+          This website is meant to bring opportunity for different artists from Batangas to showcase their artowrk and potentially get discovered. That 
+          is the reason why the website is called <span className='text-[--orange]'>Tuklas Art Gallery</span> or simply <span className='text-[--orange]'>Tuklas.</span></p>
+         
+        </div>
     </div>
+
 
   )
 }
--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import App from './App';
 import Navbar from "./components/Navbar";
 import About from './components/About';
