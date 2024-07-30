2a99ecc6c7c282bf42601754018007cc3fc13fa0
Kyoshie <157869727+kyoshie@users.noreply.github.com>
2024-07-30 02:43:06 +0000

    minor changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <div className='text-white'>
             <div className='max-w-[800px] my-[-120px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
                 <p className='text-[--orange] font-bold p-2' >DISCOVERING THE BEAUTY OF ARTS</p>
-                <h1 className='text-4xl font-bold md:text-7xl sm:text-6xl md:py-6 '>Tuklas Art Gallery.</h1>
+                <h1 className='text-4xl font-bold md:text-7xl sm:text-6xl md:py-6 '>Tuklas Art Gallery</h1>
 
                 <div className='flex items-center justify-center'>
                     <p className='py-4 text-xl font-bold md:text-3xl sm:text-4xl'>Explore the World of Art and</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React,{ useState } from 'react';
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
 
-
 const Navbar = () => {
     const [nav, setNav] = useState(false)
 
