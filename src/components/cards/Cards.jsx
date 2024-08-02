449ef9cb66790010269ede7f479502dcf553339c
Jedddy <jedbalita25@gmail.com>
2024-08-02 10:15:48 +0800

    fix card

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -7,9 +7,9 @@ const Cards = () => {
   const [cards, setCards] = useState(data);
 
   return (
-      <div>
+      <div  className="h-[90dvh] w-screen lg:items-center md:items-center lg:flex lg:flex-wrap lg:justify-center md:flex md:flex-wrap md:justify-center grid grid-cols-2">
         {cards.map((item, index) => (
-          <div key={index} className='mt-3 mr-3 transition-transform duration-300 bg-transparent border border-gray-500 rounded-lg shadow-lg hover:-translate-y-3 '>
+          <div key={index} className='md:w-[19rem] h-min m-1 transition-transform duration-300 bg-transparent border border-gray-500 rounded-lg shadow-lg hover:-translate-y-3 '>
             <img src={item.image} alt={item.name} className='w-full h-[150px] object-cover rounded-t-lg md:h-[200px] lg:h-[200px]'></img>
             <div>
               <p className='font-bold text-center text-white font-oxygen'>{item.name}</p>
--- src/pages/Marketplace.jsx
+++ src/pages/Marketplace.jsx
@@ -1,19 +1,12 @@
-import React from 'react';
-import Cards from '../components/cards/Cards';
-
+import React from "react";
+import Cards from "../components/cards/Cards";
 
 const Marketplace = () => {
-  return (
-   <div className='bg-[#222831] h-[100vh] w-[100vw] overflow-y-hidden '>
-        <div className='h-[90dvh] overflow-y-scroll p-[2em] lg:flex lg:flex-wrap lg:justify-center md:flex md:flex-wrap md:justify-center grid grid-cols-2'>
-                  <Cards/>
-                  <Cards/>
+    return (
+        <div className="bg-[#222831] h-[100vh] overflow-hidden">
+            <Cards />
         </div>
-       
-    </div>
-  )
-}
+    );
+};
 
-export default Marketplace
-
-
+export default Marketplace;
