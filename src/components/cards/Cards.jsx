42f413e63b339d373be28d9b5c428b74f6061238
kyoshie <2202901@ub.edu.ph>
2024-08-03 07:06:34 +0800

    fixed width in a large screen of the navbar so the scroll will be fixed

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -7,7 +7,7 @@ const Cards = () => {
   const [cards, setCards] = useState(data);
 
   return (
-      <div  className="h-[90dvh] w-screen lg:items-center md:items-center lg:flex lg:flex-wrap lg:justify-center md:flex md:flex-wrap md:justify-center grid grid-cols-2">
+      <div  className="h-[90dvh] w-screen lg:items-center md:items-center lg:flex lg:flex-wrap lg:justify-center md:flex md:flex-wrap md:justify-center grid grid-cols-2 overflow-y-auto lg:overflow-y-auto">
         {cards.map((item, index) => (
           <div key={index} className='md:w-[19rem] h-min m-1 transition-transform duration-300 bg-transparent border border-gray-500 rounded-lg shadow-lg hover:-translate-y-3 '>
             <img src={item.image} alt={item.name} className='w-full h-[150px] object-cover rounded-t-lg md:h-[200px] lg:h-[200px]'></img>
