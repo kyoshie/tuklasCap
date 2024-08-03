9f7f8f3d123407ac2124f48f47f0b6c132ed4dfe
kyoshie <2202901@ub.edu.ph>
2024-08-03 15:18:11 +0800

    scroll

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -7,7 +7,8 @@ const Cards = () => {
   const [cards, setCards] = useState(data);
 
   return (
-      <div  className="h-[90dvh] w-screen lg:items-center md:items-center lg:flex lg:flex-wrap lg:justify-center md:flex md:flex-wrap md:justify-center grid grid-cols-2 overflow-y-auto lg:overflow-y-scroll gap-2">
+      <div  className="h-[90dvh] w-screen lg:items-center md:items-center lg:flex lg:flex-wrap lg:justify-center md:flex md:flex-wrap md:justify-center grid grid-cols-2
+      lg:w-[100vw] overflow-y-scroll lg:overflow-y-scroll gap-2">
         {cards.map((item, index) => (
           <div key={index} className='md:w-[19rem] h-min m-1 transition-transform duration-300 bg-transparent border border-gray-500 rounded-lg shadow-lg '>
             <img src={item.image} alt={item.name} className='w-full h-[150px] object-cover rounded-t-lg md:h-[200px] lg:h-[200px]'></img>
